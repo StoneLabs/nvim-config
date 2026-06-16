8c2d50eb228b4bfb87f6911b0109aa4116674e19
Satellite Girl 🛰️ <levyehrstein@googlemail.com>
2026-06-16 18:23:27 +0900

    add undo tree, markdown

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,12 @@
+return {
+  "mbbill/undotree",
+  cmd = "UndotreeToggle",
+  config = function()
+    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.g.undotree_WindowLayout = 2
+    vim.g.undotree_SplitWidth = 40
+  end,
+  keys = {
+    { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Undo tree" },
+  },
+}
